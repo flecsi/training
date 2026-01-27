05dc97845ddcdba534f089e5134e6548f3578ec5
Julien Loiseau <jloiseau@lanl.gov>
2026-01-26 21:51:23 -0700

    Simplify cycle_control

--- solution_2/spec/control.cc
+++ solution_2/spec/control.cc
@@ -7,15 +7,9 @@ control_policy::cycle_control(control_policy &cp) {
 
   auto &s = cp.state();
 
-  s.cur.prg.t += std::min(s.par.dt, s.par.t_final - s.cur.prg.t);
+  s.cur.prg.t += s.par.dt;
+  s.cur.prg.step += 1;
 
-  if(s.cur.prg.t >= s.par.t_final) {
-    flog(info) << "step: " << s.cur.prg.step << " time: " << s.cur.prg.t
-               << " dt: " << s.par.dt << std::endl;
-    flecsi::flog::flush();
-  } // if
-
-  ++s.cur.prg.step;
   return s.cur.prg.t < s.par.t_final;
 } // cycle_control
 
